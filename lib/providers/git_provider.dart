9ccac8ed70bce8e0c4eca0ff4d60d5b8ca8c535c
jyoti <mandaljyoti654@gmail.com>
2026-07-08 16:52:53 +0545

    work on repoScreen & repoClickScreen

--- lib/providers/git_provider.dart
+++ lib/providers/git_provider.dart
@@ -82,19 +82,113 @@ class GitProvider extends ChangeNotifier {
   Color getLanguageColor(String language) {
     switch (language) {
       case "Dart":
-        return Colors.blue;
-      case "JavaScript":
-        return Colors.yellow;
-      case "HTML":
-        return Colors.orange;
-      case "CSS":
-        return Colors.purple;
+        return Color(0xff00B4AB);
+
       case "Java":
-        return Colors.red;
+        return Color(0xffB07219);
+
+      case "JavaScript":
+        return Color(0xffF1E05A);
+
+      case "TypeScript":
+        return Color(0xff3178C6);
+
       case "Python":
-        return Colors.green;
+        return Color(0xff3572A5);
+
+      case "C":
+        return Color(0xff555555);
+
+      case "C++":
+        return Color(0xffF34B7D);
+
+      case "C#":
+        return Color(0xff178600);
+
+      case "Go":
+        return Color(0xff00ADD8);
+
+      case "Kotlin":
+        return Color(0xffA97BFF);
+
+      case "Swift":
+        return Color(0xffFA7343);
+
+      case "Rust":
+        return Color(0xffDEA584);
+
+      case "Ruby":
+        return Color(0xffCC342D);
+
       case "PHP":
-        return Colors.indigo;
+        return Color(0xff4F5D95);
+
+      case "HTML":
+        return Color(0xffE34C26);
+
+      case "CSS":
+        return Color(0xff563D7C);
+
+      case "SCSS":
+        return Color(0xffC6538C);
+
+      case "Shell":
+        return Color(0xff89E051);
+
+      case "PowerShell":
+        return Color(0xff012456);
+
+      case "Objective-C":
+        return Color(0xff438EFF);
+
+      case "R":
+        return Color(0xff198CE7);
+
+      case "Lua":
+        return Color(0xff000080);
+
+      case "Perl":
+        return Color(0xff0298C3);
+
+      case "Haskell":
+        return Color(0xff5E5086);
+
+      case "Scala":
+        return Color(0xffDC322F);
+
+      case "Elixir":
+        return Color(0xff6E4A7E);
+
+      case "Clojure":
+        return Color(0xffDB5855);
+
+      case "Vue":
+        return Color(0xff41B883);
+
+      case "Svelte":
+        return Color(0xffFF3E00);
+
+      case "Assembly":
+        return Color(0xff6E4C13);
+
+      case "Jupyter Notebook":
+        return Color(0xffDA5B0B);
+
+      case "Dockerfile":
+        return Color(0xff384D54);
+
+      case "SQL":
+        return Color(0xff336791);
+
+      case "Markdown":
+        return Color(0xff083FA1);
+
+      case "YAML":
+        return Color(0xffCB171E);
+
+      case "JSON":
+        return Color(0xff292929);
+
       default:
         return Colors.grey;
     }
@@ -122,8 +216,7 @@ class GitProvider extends ChangeNotifier {
   Future<void> getRepo(String username) async {
     try {
       print("Fetching repos for: $username");
-      Response response = await dio.get(
-        "https://api.github.com/users/$username/repos",
+      Response response = await dio.get("https://api.github.com/users/$username/repos",
         options: Options(
           headers: {
             "Authorization": "Bearer $accessToken",
@@ -214,6 +307,17 @@ class GitProvider extends ChangeNotifier {
       throw Exception('Could not lunch $url ');
     }
   }
+
+  Future<void> repoWebsite(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (!await launchUrl(
+      uri,
+      mode: LaunchMode.externalApplication,
+    )) {
+      throw Exception('Could not launch $url');
+    }
+  }
 }
+
 
 //language url = https://api.github.com/repos/Nimeshis/backend/languages
