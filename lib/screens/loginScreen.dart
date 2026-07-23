63c7c6dfd9e0f3210a06002bc153b95a1f6023dc
jyoti <mandaljyoti654@gmail.com>
2026-07-23 16:57:53 +0545

    supabase login with google

--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -1,86 +1,254 @@
 import 'package:flutter/material.dart';
-import 'package:git_ums/providers/authProvider.dart';
-import 'package:git_ums/providers/git_provider.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 
+import '../providers/authProvider.dart';
 import '../routes/app_routes.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   LoginScreen({super.key});
 
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
   @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login"),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(24),
-        child: Column(
-          children: [
-            SizedBox(height: 20),
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                labelText: "Email",
-                border: OutlineInputBorder(),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.white,
+              Colors.blue,
+              Colors.black,
+              Colors.blue,
+              Colors.white,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(16),
+              child: Container(
+                padding: EdgeInsets.all(24),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(25),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 15,
+                      offset: Offset(0, 8),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Icon(
+                      Icons.lock_outline,
+                      size: 70,
+                      color: Color(0xff4A00E0),
+                    ),
+
+                    SizedBox(height: 15),
+
+                    Text(
+                      "Welcome Back",
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    SizedBox(height: 8),
+
+                    Text(
+                      "Login to continue",
+                      style: TextStyle(color: Colors.grey, fontSize: 15),
+                    ),
+
+                    SizedBox(height: 30),
+
+                    TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        prefixIcon: Icon(Icons.email_outlined),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(height: 20),
+
+                    TextField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        prefixIcon: Icon(Icons.lock_outline),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(height: 30),
+
+                    SizedBox(
+                      width: double.infinity,
+                      height: 55,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xff4A00E0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                        onPressed: () async {
+                          final auth = context.read<AuthProvider>();
+
+                          final error = await auth.login(
+                            emailController.text.trim(),
+                            passwordController.text.trim(),
+                          );
+                          if (error == null) {
+                            Navigator.pushReplacementNamed(
+                              context,
+                              AppRoutes.main,
+                            );
+                          } else {
+                            ScaffoldMessenger.of(
+                              context,
+                            ).showSnackBar(SnackBar(content: Text(error)));
+                          }
+                        },
+                        child: Text(
+                          "Login",
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(height: 20),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Don't have an account?",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, AppRoutes.signup);
+                          },
+                          child: Text(
+                            "Create Account",
+                            style: TextStyle(
+                              color: Color(0xff4A00E0),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    Divider(height: 24),
+                    // Text(
+                    //   "OR",
+                    //   style: TextStyle(color: Colors.grey),
+                    // ),
+                    //
+                    // SizedBox(height: 20),
+                    OutlinedButton.icon(
+                      style: OutlinedButton.styleFrom(
+                        minimumSize: Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      onPressed: () {},
+                      icon: FaIcon(
+                        FontAwesomeIcons.github,
+                        color: Colors.black,
+                        size: 24,
+                      ),
+                      label: Text("Continue with GitHub"),
+                    ),
+
+                    SizedBox(height: 15),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: OutlinedButton(
+                        onPressed: () async {
+                          final auth = context.read<AuthProvider>();
+
+                          final success = await auth.continueWithGoogle();
+
+                          if (success) {
+                            Navigator.pushReplacementNamed(
+                              context,
+                              AppRoutes.main,
+                            );
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text("Google sign-in failed")),
+                            );
+                          }
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              "Assets/google.png",
+                              width: 22,
+                              height: 22,
+                            ),
+                            SizedBox(width: 8),
+                            Flexible(
+                              child: Text(
+                                "Continue with Google",
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    OutlinedButton.icon(
+                      style: OutlinedButton.styleFrom(
+                        minimumSize: Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      onPressed: () {},
+                      icon: Icon(Icons.facebook, color: Colors.blue, size: 24),
+                      label: Text("Continue with Facebook"),
+                    ),
+                  ],
+                ),
               ),
             ),
-            SizedBox(height: 24,),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: "Password",
-                border: OutlineInputBorder(),
-              ),
-            ),
-
-            SizedBox(height: 20),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                 ElevatedButton(
-                  onPressed: () async {
-                    final auth =context.read<AuthProvider>();
-                    final error =await auth.login(
-                      emailController.text.trim(),
-                      passwordController.text.trim(),
-                    );
-                    if (error == null){
-                      Navigator.pushNamed(context, AppRoutes.main);
-                    }else{
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text(error)),
-                      );
-                    }
-                  },
-                  child: Text("Login"),
-                ),
-                SizedBox(width: 20,),
-                ElevatedButton(
-                  onPressed: () async {
-                    final auth =context.read<AuthProvider>();
-                    final error =await auth.signUp(
-                      emailController.text.trim(),
-                      passwordController.text.trim(),
-                    );
-
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text(error?? 'Account created successfully')),
-                    );
-                  },
-                  child: Text("Sign up"),
-                ),
-              ],
-
-            ),
-
-          ],
+          ),
         ),
       ),
     );
